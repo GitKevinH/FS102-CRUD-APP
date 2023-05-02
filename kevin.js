1a311b5060d8a5c7518538909365603566e08338
Kevin Hoang <kevin.hoang199@gmail.com>
2023-05-02 11:14:10 -0400

    template object created

---
+++ kevin.js
@@ -0,0 +1,11 @@
+//Template object
+
+const dummyData = [
+    {
+      id: 0,
+      content: "this is a post",
+      date: "01-01-2000",
+      author: "Stephen King",
+      tags: ["tag1", "tag2"],
+    },
+  ];
