33b582d77a3003098681ffd97d26585a179ed796
Kevin Hoang <kevin.hoang199@gmail.com>
2023-05-02 14:01:54 -0400

    first commit to test function

--- kevin.js
+++ kevin.js
@@ -6,6 +6,26 @@ const dummyData = [
       content: "this is a post",
       date: "01-01-2000",
       author: "Stephen King",
-      tags: ["tag1", "tag2"],
+      tags: ["tag1", "tag2","tag3"],
     },
   ];
+
+//read functionality
+
+function getPost(dataArray){
+    
+    // dataArray.forEach(post => {
+    //     post.tags.forEach(tag => {
+            
+    //     });
+    // });
+    dataArray.forEach(post => {
+    let formatPost = `Post ID: ${dataArray().id} Author: ${dataArray.author} Tags: ${dataArray.tags}  `
+    console.log(formatPost);
+    return formatPost
+        
+    });
+    
+    
+}
+getPost(dummyData);
