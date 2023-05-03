8204ace807a6e981c00f5c8f7716354c0615f220
kennedylanier <kennedylanier@gmail.com>
2023-05-03 14:06:07 -0400

    changes

--- errorValidation.js
+++ errorValidation.js
@@ -1,25 +1,38 @@
-const form = document.querySelector('form');
-const usernameInput = document.querySelector('#username');
-const postBodyInput = document.querySelector('#postBody');
-const tagsInput = document.querySelector('#tags');
+const form = document.querySelector("#createPost");
+const usernameInput = document.querySelector("#username");
+const postBodyInput = document.querySelector("#postBody");
+const tagsInput = document.querySelector("#tags");
+const errorContainer = document.querySelector("#error-message-container");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const requiredInputs = [usernameInput, postBodyInput, tagsInput];
 
-  requiredInputs.forEach(input => {
-    if (input.value.trim() === "") {
-      const errorMessageContainer = document.querySelector('#error-message-container');
-      errorMessageContainer.innerHTML = 'Please fill out all required fields.';
-      input.classList.add('is-invalid');
-    } else {
-      input.classList.remove('is-invalid');
-    }
-  });
+  const username = usernameInput.value;
+  const postBody = postBodyInput.value;
+  const tags = tagsInput.value;
 
-  if (!document.querySelectorAll('.is-invalid').length) {
-    form.submit();
+  // Validate username length
+  if (username.length < 3 || username.length > 20) {
+    errorContainer.textContent = "Username must be between 3 and 20 characters";
+    return;
   }
+
+  // Validate postBody length
+  if (postBody.length < 10 || postBody.length > 500) {
+    errorContainer.textContent = "Post body must be between 10 and 500 characters";
+    return;
+  }
+
+  // Validate tags
+  const tagsRegex = /^[a-zA-Z0-9, ]*$/;
+  if (!tagsRegex.test(tags)) {
+    errorContainer.textContent = "Tags can only contain letters, numbers, commas, and spaces";
+    return;
+  }
+
+  // If all validations pass, submit the form
+  errorContainer.textContent = "";
+  form.submit();
 });
 
-  
+
